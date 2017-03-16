0ea187f4aa6f8230981f14d6d16a855f8d85e8a9
afeiship <1290657123@qq.com>
2017-03-16 13:08:07 +0800

    intial

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = function (inValue,inNow) {
+  if (inValue) {
+    var now = inNow ? new Date(inNow): new Date();
+    var publish = (new Date(String(inValue).replace(/-/g, '/')));
+    var dayTs = 24 * 60 * 60 * 1000;
+    var nowTs = now.getTime();
+    var publishTs = publish.getTime();
+    var isToday = publish.getDate() == now.getDate();
+    var isYesterday = (new Date(nowTs - dayTs)).getDate() == publish.getDate();
+    var isMonth = publish.getMonth() == now.getMonth();
+    var isYear = publish.getYear() == now.getYear();
+    var diff_s = (nowTs - publishTs) / 1000;
+    var diff_m = diff_s / 60;
+    var diff_h = diff_m / 60;
+
+
+    switch (true) {
+      case diff_s < 60:
+        return '刚刚';
+      case diff_m < 60:
+        return Math.floor(diff_m) + '分钟前';
+      case isToday && diff_h < 24:
+        return Math.floor(diff_h) + '小时前';
+      case isYear && isMonth && isYesterday:
+        return '昨天 ' + inValue.slice(11, -3);
+      case isYear:
+        return inValue.slice(-14, -3);
+      default:
+        return inValue.slice(0, 10);
+    }
+  }
+  return inValue;
+};
---
+++ test/test.js
@@ -0,0 +1,62 @@
+var assert = require('assert');
+var timeago=require('../index');
+
+/**
+以下顺序选择显示规则：
+1、60秒内：刚刚；
+2、60分钟内：XX分钟前；
+3、今天：X小时前
+4、昨天：昨天 04:25
+5、今年内：1-22 02:23
+6、往年的：2010-1-22
+ */
+describe('timeago', function () {
+
+  describe('以下顺序选择显示规则：', function () {
+    it('1、60秒内：刚刚:', function () {
+      var time_pub = '2017-03-16 12:58:51';
+      var time_now = '2017-03-16 12:57:52';
+      var result = timeago(time_pub,time_now);
+      assert.equal('刚刚',result);
+    });
+
+    it('2、60分钟内：XX分钟前:', function () {
+      var time_pub = '2017-03-16 11:58:51';
+      var time_now = '2017-03-16 12:57:52';
+      var result = timeago(time_pub,time_now);
+      assert.equal('59分钟前',result);
+    });
+
+    it('3、今天：X小时前:', function () {
+      var time_pub = '2017-03-16 11:58:51';
+      var time_now = '2017-03-16 23:59:00';
+      var result = timeago(time_pub,time_now);
+      assert.equal('12小时前',result);
+    });
+
+
+    it('4、昨天：昨天 04:25:', function () {
+      var time_pub = '2017-03-15 11:58:51';
+      var time_now = '2017-03-16 23:59:00';
+      var result = timeago(time_pub,time_now);
+      assert.equal('昨天 11:58',result);
+    });
+
+
+    it('5、今年内：1-22 02:23:', function () {
+      var time_pub = '2017-01-01 00:01:51';
+      var time_now = '2017-03-16 23:59:00';
+      var result = timeago(time_pub,time_now);
+      assert.equal('01-01 00:01',result);
+    });
+
+    it('6、往年的：2010-1-22:', function () {
+      var time_pub = '2010-01-01 00:01:51';
+      var time_now = '2017-03-16 23:59:00';
+      var result = timeago(time_pub,time_now);
+      assert.equal('2010-01-01',result);
+    });
+
+  });
+
+});
